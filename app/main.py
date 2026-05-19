4833322c4f034e40b78f714c50fda2881d46bb36
Dima-shima <shifrin1970@gmail.com>
2026-05-19 15:49:08 +0300

    implemented tests

--- app/main.py
+++ app/main.py
@@ -18,6 +18,22 @@ def get_human_age(cat_age: int, dog_age: int) -> list:
         get_human_age(15, 15) == [1, 1]
         get_human_age(24, 24) == [2, 2]
     """
-    # TODO: Implement this function
-    # Write your tests first, then implement the logic
-    return [0, 0]
+    if not isinstance(cat_age, int) or not isinstance(dog_age, int):
+        raise ValueError("Arguments must be integer")
+    if not cat_age >= 0 or not dog_age >= 0:
+        raise ValueError("Arguments must be Not negative")
+    if not cat_age <= 100 or not dog_age <= 100:
+        raise ValueError("Arguments must be Not very big")
+    if 0 <= cat_age <= 14:
+        cat_human_age = 0
+    if 0 <= dog_age <= 14:
+        dog_human_age = 0
+    if 15 <= cat_age < 24:
+        cat_human_age = 1
+    if 15 <= dog_age < 24:
+        dog_human_age = 1
+    if 24 <= cat_age:
+        cat_human_age = int(2 + (cat_age - 24)/4)
+    if 24 <= dog_age:
+        dog_human_age = int(2 + (cat_age - 24)/5)
+    return[cat_human_age, dog_human_age]
--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,35 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+class TestHumanAgeCalculate:
+
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, expected",
+        [
+            pytest.param(0, 0, [0, 0], id="should add zeros when cat_dog just born"),
+            pytest.param(15, 15, [1, 1], id="should add one year young"),
+            pytest.param(23, 23, [1, 1], id="should add one year elderly"),
+            pytest.param(24, 24, [2, 2], id="should add two year"),
+            pytest.param(28, 28, [3, 2], id="should add three year"),
+            pytest.param(100, 100, [21, 17], id="should add many years"),
+        ]
+    )
+    def test_modify(self, cat_age, dog_age, expected):
+        assert get_human_age(cat_age, dog_age) == expected
+
+
+class TestRaisingErrors:
+
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, expected_error",
+        [
+            pytest.param("12", 12, ValueError, id="should raise error if arguments Not integer"),
+            pytest.param(-12, 12, ValueError, id="should raise error if arguments Negative"),
+            pytest.param(112, 12, ValueError, id="should raise error if arguments Very big"),
+        ]
+    )
+    def test_raising_errors_correctly(self, cat_age, dog_age, expected_error):
+        with pytest.raises(expected_error):
+            get_human_age(cat_age, dog_age)
